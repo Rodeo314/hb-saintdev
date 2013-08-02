c69e9a2ba76094cf5a66ecdbd52ab9aea6c205ca
Tim Walker <tdskywalker@gmail.com>
2013-08-02 16:13:19 +0200

    QSV: create a dedicated option for "AsyncDepth".
    This parameter applies to all of decode, filtering and encode,
    whereas the'--encopts' string is for encoding-specific options.

--- libhb/common.c
+++ libhb/common.c
@@ -2832,6 +2832,7 @@ static void job_setup( hb_job_t * job, hb_title_t * title )
 
 #ifdef USE_QSV
     job->qsv_decode = title->qsv_decode_support;
+    job->qsv_async_depth = AV_QSV_ASYNC_DEPTH_DEFAULT;
 #endif
 }
 
