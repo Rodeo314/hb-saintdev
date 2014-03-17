233387447724a86c1270fcc42084402276dac194
Tim Walker <tdskywalker@gmail.com>
2014-03-17 20:09:06 +0100

    Fix H.265 header

--- libhb/h265_common.h
+++ libhb/h265_common.h
@@ -1,10 +1,11 @@
-/* h264_common.h
-
-   Copyright (c) 2003-2014 HandBrake Team
-   This file is part of the HandBrake source code
-   Homepage: <http://handbrake.fr/>.
-   It may be used under the terms of the GNU General Public License v2.
-   For full terms see the file COPYING file or visit http://www.gnu.org/licenses/gpl-2.0.html
+/*
+ * h265_common.h
+ *
+ * Copyright (c) 2003-2014 HandBrake Team
+ * This file is part of the HandBrake source code
+ * Homepage: <http://handbrake.fr/>.
+ * It may be used under the terms of the GNU General Public License v2.
+ * For full terms see the file COPYING file or visit http://www.gnu.org/licenses/gpl-2.0.html
  */
 
 #ifndef HB_H265_COMMON_H
@@ -21,4 +22,4 @@ static const char* const hb_h265_colorprim_names[] = { "", "bt709", "undef", "",
 static const char* const  hb_h265_transfer_names[] = { "", "bt709", "undef", "", "bt470m", "bt470bg", "smpte170m", "smpte240m", "linear", "log100", "log316", "iec61966-2-4", "bt1361e", "iec61966-2-1", "bt2020-10", "bt2020-12", NULL, };
 static const char* const hb_h265_colmatrix_names[] = { "GBR", "bt709", "undef", "", "fcc", "bt470bg", "smpte170m", "smpte240m", "YCgCo", "bt2020nc", "bt2020c", NULL, };
 
-#endif  //HB_H265_COMMON_H
+#endif // HB_H265_COMMON_H
