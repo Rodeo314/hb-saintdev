1a605637f6eca873f5e3bd4455e0acd4b4166a95
Tim Walker <tdskywalker@gmail.com>
2014-03-20 21:16:38 +0100

    willnothelpbutryanyway

--- libhb/nal_units.c
+++ libhb/nal_units.c
@@ -77,9 +77,9 @@ uint8_t* hb_annexb_find_next_nalu(const uint8_t *start, size_t *size)
      * sequence == 0 too (start code emulation prevention will prevent such a
      * sequence from occurring outside of a start code prefix)
      */
-    while (end - buf > 3)
+    while (end - buf > 4)
     {
-        if (!buf[0] && !buf[1] && (!buf[2] || buf[2] == 1))
+        if (!buf[0] && !buf[1] && !buf[2] && buf[3] == 1)
         {
             end = buf;
             break;
