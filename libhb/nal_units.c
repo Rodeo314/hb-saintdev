d1d812f80883b242b4ba18b76bbdec0ef4a97653
Tim Walker <tdskywalker@gmail.com>
2014-03-20 21:57:36 +0100

    maybe

--- libhb/nal_units.c
+++ libhb/nal_units.c
@@ -38,6 +38,7 @@ size_t hb_nal_unit_write_isomp4(uint8_t *buf,
 
     if (buf != NULL)
     {
+        hb_log("hb_nal_unit_write_isomp4:       NAL with type %02"PRIu8" and size %5lu", buf[0] & 0x1f, buf_size);//debug
         memcpy(buf, &nalu_length, sizeof(nalu_length));
         memcpy(buf + sizeof(nalu_length), nal_unit, nal_unit_size);
     }
