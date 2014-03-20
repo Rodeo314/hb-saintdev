ccb492c6114d90a108d033fc1c2216ae61db2d9c
Tim Walker <tdskywalker@gmail.com>
2014-03-20 21:03:42 +0100

    More

--- libhb/nal_units.c
+++ libhb/nal_units.c
@@ -38,7 +38,7 @@ size_t hb_nal_unit_write_isomp4(uint8_t *buf,
 
     if (buf != NULL)
     {
-        hb_log("hb_nal_unit_write_isomp4: %"PRIu8" at %#p with size %lu", buf[0] & 0x1f, buf, nal_unit_size);//debug
+        hb_log("hb_nal_unit_write_isomp4: %02"PRIu8" at 0x%p with size %5lu", buf[0] & 0x1f, buf, nal_unit_size);//debug
         memcpy(buf, &nalu_length, sizeof(nalu_length));
         memcpy(buf + sizeof(nalu_length), nal_unit, nal_unit_size);
     }
