50ad775395820508d29840ee0517b7fbf8c6bbb9
Tim Walker <tdskywalker@gmail.com>
2014-04-01 17:46:52 +0200

    Improve it one last time

--- libhb/enc_qsv.c
+++ libhb/enc_qsv.c
@@ -2046,12 +2046,12 @@ int encqsvWork(hb_work_object_t *w, hb_buffer_t **buf_in, hb_buffer_t **buf_out)
      *
      * - flush the encoder to encode and retrieve any buffered frames
      *
-     * - do a hard reset (MFXVideoENCODE_Close, then Init) of the
-     *   encoder so that the next sequence start with a keyframe
+     * - do a hard reset (MFXVideoENCODE_Close, then Init) of
+     *   the encoder to make sure the next frame is a keyframe
      *
      * The hard reset ensures encoding resumes with a clean state, avoiding
-     * miscellaneous hard-to-disagnose issues that may occur when resuming an
-     * encode after flushing the encoder and/or using MFXVideoENCODE_Reset.
+     * miscellaneous hard-to-disagnose issues that may occur when resuming
+     * an encode after flushing the encoder or using MFXVideoENCODE_Reset.
      */
     if (in->s.new_chap > 0 && job->chapter_markers)
     {
