1f12419809d95d8acb49e3393cda9c49e2046ba5
Tim Walker <tdskywalker@gmail.com>
2014-04-03 16:51:13 +0200

    encqsvClose: fix a memory leak.

--- libhb/enc_qsv.c
+++ libhb/enc_qsv.c
@@ -1237,6 +1237,11 @@ void encqsvClose(hb_work_object_t *w)
 
     if (pv != NULL)
     {
+        if (pv->delayed_processing != NULL)
+        {
+            /* the list is already empty */
+            hb_list_close(&pv->delayed_processing);
+        }
         if (pv->sws_context_to_nv12 != NULL)
         {
             sws_freeContext(pv->sws_context_to_nv12);
