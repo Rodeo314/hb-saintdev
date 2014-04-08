d5e216c98cc8868124d9eb9a8ccd99ec9a5a45a7
Tim Walker <tdskywalker@gmail.com>
2014-04-08 15:33:24 +0200

    Check

--- libhb/common.c
+++ libhb/common.c
@@ -2657,7 +2657,7 @@ void hb_list_close( hb_list_t ** _l )
     hb_log("hb_list_close: 0x%p with items 0x%p", l, l->items);//debug
 
     free( l->items );
-    free( l );
+//    free( l );
 
     *_l = NULL;
 }
